905baed815518085522cda56ccf45388ed5857fe
Alex Burrell <awburrell@mc.edu>
2024-08-11 22:46:27 -0500

    Fixed shell script partially

--- camlnet/replace_inet.sh
+++ camlnet/replace_inet.sh
@@ -10,15 +10,18 @@ if [ $? != 0 ]; then
 fi
 
 echo "Do you want to reboot your minix install right now?\n[y] [n]"
-if [ "$REPLY" == "y" ] || [ "$REPLY" == "yes" ] || [ "$REPLY" == "Y" ]
-|| [ "$REPLY" == "Yes" ] || [ " $REPLY" == "YES" ]; then
-  sleep 10
+read REPLY
+if [ "$REPLY" = "y" ] || [ "$REPLY" = "yes" ] || [ "$REPLY" = "Y" ]
+|| [ "$REPLY" = "Yes" ] || [ "$REPLY" = "YES" ]; then
+  echo "Sleeping to give time for services to cleanly adjust."
+  sleep 7
   reboot
   exit 0
 fi
-if [ "$REPLY" == "n" ] || [ "$REPLY" == "no" ] || [ "$REPLY" == "N" ]
-|| [ "$REPLY" == "No" ] || [ "$REPLY" == "NO" ]; then
+if [ "$REPLY" = "n" ] || [ "$REPLY" = "no" ] || [ "$REPLY" = "N" ]
+|| [ "$REPLY" = "No" ] || [ "$REPLY" = "NO" ]; then
   echo "Script done."
   exit 0
 fi 
+echo "Command intelligible."
 exit 65
