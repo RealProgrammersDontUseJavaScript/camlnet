6bd1ef0533b0b6aa8dcdb68c4c3ef395b38b4c2e
Alex Burrell <awburrell@mc.edu>
2024-08-13 14:53:04 -0500

    Improved error handling on replace_inet

--- camlnet/replace_inet.sh
+++ camlnet/replace_inet.sh
@@ -4,22 +4,20 @@
 # Unfortunately, it doesn't work until reboot
 
 
-service down inet && rm /service/inet && cp ./inet /service/inet
+file inet && service down inet && rm /service/inet && cp ./inet /service/inet
 if [ $? != 0 ]; then
   exit 126
 fi
 
 echo "Do you want to reboot your minix install right now?\n[y] [n]"
 read REPLY
-if [ "$REPLY" = "y" ] || [ "$REPLY" = "yes" ] || [ "$REPLY" = "Y" ]
-|| [ "$REPLY" = "Yes" ] || [ "$REPLY" = "YES" ]; then
+if [ "$REPLY" = "y" ] || [ "$REPLY" = "yes" ] || [ "$REPLY" = "Y" ] || [ "$REPLY" = "Yes" ] || [ "$REPLY" = "YES" ]; then
   echo "Sleeping to give time for services to cleanly adjust."
   sleep 7
   reboot
   exit 0
 fi
-if [ "$REPLY" = "n" ] || [ "$REPLY" = "no" ] || [ "$REPLY" = "N" ]
-|| [ "$REPLY" = "No" ] || [ "$REPLY" = "NO" ]; then
+if [ "$REPLY" = "n" ] || [ "$REPLY" = "no" ] || [ "$REPLY" = "N" ] || [ "$REPLY" = "No" ] || [ "$REPLY" = "NO" ]; then
   echo "Script done."
   exit 0
 fi 
