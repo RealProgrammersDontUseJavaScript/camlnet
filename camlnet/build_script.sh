8b176acdf2d8b2e7305a1dcb9df375c0312266ff
Alex Burrell <awburrell@mc.edu>
2024-08-14 23:00:05 -0500

    Minor build_script impr. & camlnet.tar.gz iz accurate

--- camlnet/build_script.sh
+++ camlnet/build_script.sh
@@ -14,5 +14,16 @@ ocamlopt -verbose -output-obj -o jooseBox.o ./generic/tcp_lib_help.ml
 make || exit 1
 
 
-# After this, replace the version of inet
-# By running replace_inet.sh
+# After this, ask the user if they want
+# to replace the version of inet & reboot
+echo "Do you want to reboot right now?\n[y] [n]"
+read REPLY
+if [ "$REPLY" = "y" ] || [ "$REPLY" = "yes" ] || [ "$REPLY" = "Y" ] || [ "$REPLY" = "Yes" ] || [ "$REPLY" = "YES" ]; then
+	./replace_inet.sh
+fi
+if [ "$REPLY" = "n" ] || [ "$REPLY" = "no" ] || [ "$REPLY" = "N" ] || [ "$REPLY" = "No" ] || [ "$REPLY" = "NO" ]; then
+	echo "Done"
+	exit 0
+fi
+echo "Reply unintelligble."
+exit 65
